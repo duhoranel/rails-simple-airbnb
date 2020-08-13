8bee65cf1fcd1f4920a7cdd5af1fc62f6dd616d0
Laure Duhorane <laure.duhorane@lewagon.org>
2020-08-13 19:45:16 -0400

    model controller & view implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture: 'https://unsplash.com/photos/_TPTXZd9mOo'
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Paris',
+  address: '10 Clifton Gardens Paris, France',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Bujumbura',
+  address: '10 Clifton Gardens Bujumbura, Burundi',
+  description: 'A lovely summer feel for this spacious garden flat. Three double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 125,
+  number_of_guests: 6
+)
